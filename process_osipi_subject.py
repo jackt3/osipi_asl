f60fcc44e2c2b827b840f880b75388d84d8c316b
jackt3 <jackatoner@gmail.com>
2021-10-07 17:20:18 +0100

    Updates
    Added code to get data in the expected format.
    Also tweaked how the verbose argument is handled.

--- process_osipi_subject.py
+++ process_osipi_subject.py
@@ -1,8 +1,11 @@
 import subprocess
 import argparse
 from pathlib import Path
+import shutil
 import time
 import logging
+import numpy as np
+import nibabel as nb
 
 IAFS = ["diff", "tc", "ct"]
 DATASETS = {
@@ -38,12 +41,22 @@ SUBJECTS = {"sub-PopulationAverage": "population",
             "sub-DRO8": "synthetic",
             "sub-DRO9": "synthetic"}
 
+def masked_std(image_name, mask_name):
+    # load image and mask
+    image = nb.load(image_name)
+    mask = nb.load(mask_name)
+
+    # set values to NaN outside of mask
+    nanned_image = np.where(mask.get_fdata()==1, image.get_fdata(), np.nan)
+
+    return np.nanstd(nanned_image)
+
 def run_cmd(subid, cmd, shell=False):
     logger = logging.getLogger(subid)
+    logger.info(" ".join(cmd))
     if shell:
-        process = subprocess.Popen(" ".join(cmd), shell=True, stdout=subprocess.PIPE)
-    else:
-        process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        cmd = " ".join(cmd)
+    process = subprocess.Popen(cmd, shell=shell, stdout=subprocess.PIPE)
     while 1:
         retcode = process.poll()
         line = process.stdout.readline().decode("utf-8")
@@ -54,13 +67,16 @@ def run_cmd(subid, cmd, shell=False):
         logger.info(f"retcode={retcode}")
         logger.exception("Process failed.")
 
+def imcp_wrapper(name1, name2):
+    imcp_cmd = ["imcp", str(name1), str(name2)]
+    run_cmd(imcp_cmd)
+
 def process_subject(study_dir, subid, intermediate_dir="", spatial=True, verbose=False, debug=False):
     #############################################################################
     ############################## general set up ###############################
     #############################################################################
-    # get pipeline start time if verbose mode is enabled
-    if verbose:
-        start_time = time.time()
+    # get pipeline start time
+    start_time = time.time()
 
     # check subject's directory exists in `study_dir`
     study_dir = Path(study_dir).resolve(strict=True)
@@ -86,14 +102,12 @@ def process_subject(study_dir, subid, intermediate_dir="", spatial=True, verbose
         logger.addHandler(handler)
 
     # check `anat` and `perf` directories exist
-    if verbose:
-        logger.info("Checking `anat` and `perf` directories exist.")
+    logger.info("Checking `anat` and `perf` directories exist.")
     anat_dir = (sub_dir/"anat").resolve(strict=True)
     perf_dir = (sub_dir/"perf").resolve(strict=True)
 
     # find subject's images
-    if verbose:
-        logger.info("Checking necessary images exist.")
+    logger.info("Checking necessary images exist.")
     t1 = (anat_dir/f"{subid}_T1w.nii.gz").resolve(strict=True)
     asl = (perf_dir/f"{subid}_asl.nii.gz").resolve(strict=True)
     m0 = (perf_dir/f"{subid}_m0scan.nii.gz").resolve(strict=True)
@@ -111,9 +125,9 @@ def process_subject(study_dir, subid, intermediate_dir="", spatial=True, verbose
     logger.info("Converting any NaNs in the ASL image to 0.")
     asl_nonan = intermediate_dir/f"{subid}_asl_nonan.nii.gz"
     fslmaths_cmd = ["fslmaths",
-                    asl,
+                    str(asl),
                     "-nan",
-                    asl_nonan]
+                    str(asl_nonan)]
     run_cmd(subid, fslmaths_cmd)
 
     # if debug mode is enabled, also produce a mask of where the NaN 
@@ -122,9 +136,9 @@ def process_subject(study_dir, subid, intermediate_dir="", spatial=True, verbose
         logger.info("Producing a mask which is 1 at locations where the ASL image has NaNs.")
         asl_nanmask = intermediate_dir/f"{subid}_asl_nanmask.nii.gz"
         fslmaths_cmd = ["fslmaths",
-                        asl,
+                        str(asl),
                         "-nanm",
-                        asl_nanmask]
+                        str(asl_nanmask)]
         run_cmd(subid, fslmaths_cmd)
 
     # get scan parameters
@@ -134,10 +148,9 @@ def process_subject(study_dir, subid, intermediate_dir="", spatial=True, verbose
     #############################################################################
     ################################ fsl_anat section ###########################
     #############################################################################
-    # get start time if verbose mode is enabled
-    if verbose:
-        logger.info("Running fsl_anat.")
-        fsl_anat_start = time.time()
+    # get start time
+    logger.info("Running fsl_anat.")
+    fsl_anat_start = time.time()
     
     # create fsl_anat command
     fsl_anat_out = intermediate_dir/f"{subid}_T1w"
@@ -152,17 +165,12 @@ def process_subject(study_dir, subid, intermediate_dir="", spatial=True, verbose
     if scan_type is "population":
         fsl_anat_cmd.append("--nocrop")
 
-    # print fsl_anat command if verbose mode is enabled
-    if verbose:
-        logger.info(" ".join(fsl_anat_cmd))
-
     # run fsl_anat command
     run_cmd(subid, fsl_anat_cmd)
 
-    # print fsl_anat run time to terminal if verbose mode is enabled
-    if verbose:
-        fsl_anat_end = time.time()
-        logger.info(f"fsl_anat completed in {(fsl_anat_end-fsl_anat_start)/60:2f} minutes.")
+    # get fsl_anat run time
+    fsl_anat_end = time.time()
+    logger.info(f"fsl_anat completed in {(fsl_anat_end-fsl_anat_start)/60:2f} minutes.")
     
     # append '.anat' to fsl_anat output directory name
     fsl_anat_out = fsl_anat_out.with_suffix(".anat")
@@ -211,29 +219,74 @@ def process_subject(study_dir, subid, intermediate_dir="", spatial=True, verbose
     if not spatial:
         oxford_asl_cmd.append("--spatial=off")
     
-    # print oxford_asl command to terminal and get start time 
-    # if verbose mode is enabled
-    if verbose:
-        logger.info("Running oxford_asl.")
-        oxford_asl_start = time.time()
+    # print oxford_asl command to terminal and get start time
+    logger.info("Running oxford_asl.")
+    oxford_asl_start = time.time()
     
     # run oxford_asl command
     run_cmd(subid, oxford_asl_cmd, shell=True)
 
-    # print oxford_asl run time if verbose mode is enabled
-    if verbose:
-        oxford_asl_end = time.time()
-        logger.info(f"oxford_asl completed in {(oxford_asl_end-oxford_asl_start)/60:2f} minutes.")
+    # print oxford_asl run time
+    oxford_asl_end = time.time()
+    logger.info(f"oxford_asl completed in {(oxford_asl_end-oxford_asl_start)/60:2f} minutes.")
     
     #############################################################################
     ############################## clean up section #############################
     #############################################################################
-    # move files to locations expected for submission to OSIPI
+    logger.info("Getting files into format expected for OSIPI submission.")
+
+    ### non partial volume corrected results
+    # move perfusion_calib to location expected for submission to OSIPI
+    logger.info("Copying non-partial volume corrected results to expected locations.")
+    native_dir = oxford_asl_out/"native_space"
+    perfusion_calib = native_dir/"perfusion_calib.nii.gz"
+    imcp_wrapper(perfusion_calib, results_dir/"CBF.nii.gz")
+
+    # get GM and WM masks and move to expected location
+    gm_mask, wm_mask = [native_dir/f"{t}_roi.nii.gz" for t in ("gm", "wm")]
+    for mask_src, tissue in zip((gm_mask, wm_mask), ("GM", "WM")):
+        imcp_wrapper(mask_src, results_dir/f"{tissue}_mask_lowres.nii.gz")
+
+    # names of mean ROI estimates
+    gm_mean, wm_mean = [native_dir/f"perfusion_calib_{t}_mean.txt" for t in ("gm", "wm")]
+
+    # calculate standard deviation within tissue mask and write mean and std to file
+    for t_mean, t_mask, t in zip((gm_mean, wm_mean), (gm_mask, wm_mask), ("GM", "WM")):
+        echo_cmd = ["echo",  f"\"$(cat {str(t_mean)})\"",  ">", str(results_dir/f"{t}_CBF.txt")]
+        run_cmd(subid, echo_cmd, shell=True)
+        std_cmd = ["echo", f"\"$(fslstats {str(perfusion_calib)} -k {str(t_mask)} -S)\"", ">>", str(results_dir/f"{t}_CBF.txt")]
+        run_cmd(subid, std_cmd, shell=True)
+    
+    ### partial volume corrected results
+    # move partial volume corrected GM and WM perfusion estimates
+    logger.info("Copying partial volume corrected results and partial volume estimates to expected locations.")
+    pvcorr_dir = native_dir/"pvcorr"
+    pgm_perfusion_calib = pvcorr_dir/"perfusion_calib.nii.gz"
+    imcp_wrapper(pgm_perfusion_calib, results_dir/"CBF_GMpv.nii.gz")
+    pwm_perfusion_calib = pvcorr_dir/"perfusion_wm_calib.nii.gz"
+    imcp_wrapper(pwm_perfusion_calib, results_dir/"CBF_WMpv.nii.gz")
+
+    # move T1w space PVEs
+    gm_pve, wm_pve = [fsl_anat_out/f"T1_fast_pve_{n}" for n in (0, 1)]
+    for pve, t in zip((gm_pve, wm_pve), ("GM", "WM")):
+        imcp_wrapper(pve, results_dir/f"{t}_pv.nii.gz")
+
+    # calculate standard deviation within tissue mask and write mean and std to file
+    pgm_mean, pwm_mean = [native_dir/f"perfusion_{t}_mean.txt" for t in ("calib_gm", "wm_calib_wm")]
+    for t_perf, t_mean, t_mask, t in zip((pgm_perfusion_calib, pwm_perfusion_calib), (pgm_mean, pwm_mean), (gm_mask, wm_mask), ("GM", "WM")):
+        echo_cmd = ["echo",  f"\"$(cat {str(t_mean)})\"",  ">", str(results_dir/f"{t}_CBF.txt")]
+        run_cmd(subid, echo_cmd, shell=True)
+        std_cmd = ["echo", f"\"$(fslstats {str(t_perf)} -k {str(t_mask)} -S)\"", ">>", str(results_dir/f"{t}pv_CBF.txt")]
+        run_cmd(subid, std_cmd, shell=True)
+
+    # if not in debug mode, delete intermediate_results directory
+    if not debug:
+        logger.info("Cleaning up intermediate files.")
+        shutil.rmtree(intermediate_dir)
 
     # end of processing pipeline!
-    if verbose:
-        end_time = time.time()
-        logger.info(f"Processing completed! Time taken = {(end_time - start_time)/60:2f} minutes.")
+    end_time = time.time()
+    logger.info(f"Processing completed! Time taken = {(end_time - start_time)/60:2f} minutes.")
 
 if __name__ == '__main__':
     # argument handling
